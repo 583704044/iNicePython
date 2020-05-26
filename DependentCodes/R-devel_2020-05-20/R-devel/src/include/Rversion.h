d5e1e745bb4bece438f38ee872a2d36c412b805a
yjf <yjf66@qq.com>
2020-05-27 07:48:25 +0800

    init

---
+++ DependentCodes/R-devel_2020-05-20/R-devel/src/include/Rversion.h
@@ -0,0 +1,25 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 262400
+#define R_NICK "Unsuffered Consequences"
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "4"
+#define R_MINOR  "1.0"
+#define R_STATUS "Under development (unstable)"
+#define R_YEAR   "2020"
+#define R_MONTH  "05"
+#define R_DAY    "20"
+#define R_SVN_REVISION 78515
+#define R_FILEVERSION    4,10,78515,0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
