f645233fe664ea0c2344b732d3b0240273a1f991
xiterator <yjf66@qq.com>
2020-05-27 17:42:50 +0800

    gammamixEM.R comments_0

--- DependentCodes/testGammamix.R
+++ DependentCodes/testGammamix.R
@@ -7,11 +7,18 @@ out <- gammamixEM(x, lambda = c(1, 1, 1)/3, verb = TRUE)
 print(out$lambda)
 print(out$gamma.pars)
 for ( p in out$gamma.pars) {
-  print(p$alpha)
+  print(p)
 }
-  
+
+# mixtools 1.1.0 initial guess for alpha and beta are
+# Debug> alpha
+# [1]  0.3571404  6.1872099 26.8053743
+# Debug> beta
+# [1]  5.457196  4.938647 12.137255
 
 # mixtools 1.1.0 running results:
+# iteration = 5  log-lik diff = 5.848051e-10  log-lik = -2538.996 
+# number of iterations= 5 
 # $lambda
 # [1] 0.3245213 0.3421453 0.3333334
 
@@ -19,7 +26,7 @@ for ( p in out$gamma.pars) {
 #       comp.1    comp.2     comp.3
 # alpha 0.3571404 6.187210   26.80537
 # beta  5.4571960 4.938647   12.13725
-# mean: 
+# mean: 1.95      30.56      325.34
 
 # mixtools 1.2.0 running result:
 # iteration = 547  log-lik diff = 9.681571e-09  log-lik = -2511.448 
