d661af1588a562133616881707ec7a00c628c3d7
xiterator <yjf66@qq.com>
2020-06-03 18:50:50 +0800

    nlm_simple cpp implement partly

--- PersonalHome/jianfei/nlm_simple/nlm_simple.cpp
+++ PersonalHome/jianfei/nlm_simple/nlm_simple.cpp
@@ -2,24 +2,94 @@
 // Created by jianfei on 20-5-29.
 //
 
-#include <stdio.h>
 #include "nlm_simple.h"
 #include "FunctionInfo.h"
+#include <iostream>
+using namespace std;
 
-//double nlm_simple(LossFunType f, double* xInit,
-//        unsigned long n, double* xOutput, int* resultCode, int* iterCount,
-//        double *c_typsize, double c_fscale, int c_print_level,
-//        int c_ndigit, double c_gradtol,
-//        int c_stepmax, double c_steptol,
-//        int c_iterlim, bool c_check_analyticals);
-////c_typsize = [1,1,..1], length=length(xInit)
-//// c_fscale = 1, c_print_level = 0 in {0,1,2},
-//// c_ndigit = 12, c_gradtol = 1e-06,
-//// c_stepmax = max(1000 * sqrt( sum((xInit/typsize)^2) ), 1000),
-//// c_steptol = 1e-06,
-//// c_iterlim = 100,
-//// c_check_analyticals = TRUE
 
+#define FT_SIZE 5		/* default size of table to store computed function values */
+
+static void optcode(int code)
+{
+    switch(code) {
+        case 1:
+            cout << "Relative gradient close to zero.\n";
+            cout << "urrent iterate is probably solution.\n";
+            break;
+        case 2:
+            cout << "Successive iterates within tolerance.\n";
+            cout << "Current iterate is probably solution.\n";
+            break;
+        case 3:
+            cout << "Last global step failed to locate a point lower than x.\n";
+            cout << "Either x is an approximate local minimum of the function,\n\
+the function is too non-linear for this algorithm,\n\
+or steptol is too large.\n";
+            break;
+        case 4:
+            cout << "Iteration limit exceeded.  Algorithm failed.\n";
+            break;
+        case 5:
+            cout << "Maximum step size exceeded 5 consecutive times.\n\
+Either the function is unbounded below,\n\
+becomes asymptotic to a finite value\n\
+from above in some direction,\n"\
+"or stepmx is too small.\n";
+            break;
+    }
+    cout << "\n";
+}
+
+/* Fatal errors - we don't deliver an answer */
+
+static void opterror(int nerr)
+{
+    switch(nerr) {
+        case -1:
+            cout << "ERROR: non-positive number of parameters in nlm" << endl;
+            break;
+
+        case -2:
+            cout << "ERROR: nlm is inefficient for 1-d problems" << endl;
+            break;
+
+        case -3:
+            cout << "ERROR: invalid gradient tolerance in nlm" << endl;
+            break;
+
+        case -4:
+            cout << "ERROR: invalid iteration limit in nlm" << endl;
+            break;
+
+        case -5:
+            cout << "ERROR: minimization function has no good digits in nlm" << endl;
+            break;
+
+        case -6:
+            cout << "ERROR: no analytic gradient to check in nlm!" << endl;
+            break;
+
+        case -7:
+            cout << "ERROR: no analytic Hessian to check in nlm!" << endl;
+            break;
+
+        case -21:
+            cout << "probable coding error in analytic gradient" << endl;
+            break;
+
+        case -22:
+            cout << "probable coding error in analytic Hessian" << endl;
+            break;
+
+        default:
+            cout << "*** unknown error message (msg= " << nerr << ") in nlm_simple\n*** should not happen!" << endl;
+            break;
+    }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 double nlm_simple(LossFunType f, double* xInit,
         unsigned long n, double* xOutput, int* resultCode, int* iterCount,
         double *c_typsize, double c_fscale, int c_print_level,
@@ -28,138 +98,54 @@ double nlm_simple(LossFunType f, double* xInit,
         int c_iterlim, bool c_check_analyticals) {
 //return function value at the point xOutput
 
+    //
+    //msg
+    //
     if (c_print_level < 0) {
         c_print_level = 0;
     }
     else if (c_print_level > 2) {
         c_print_level = 2;
     }
-    int msgArr[] = {9, 1, 16};
+    const int msgArr[] = {9, 1, 17};  //msg <- (1 + c(8, 0, 16))[1 + print.level]
+    int msg = msgArr[c_print_level];
+    int omsg = msg;                  //keep a copy to used after calling optif9
+    if (!c_check_analyticals)
+        msg += (2 + 4);
 
+    //
+    // analytic gradient, hessian
+    //
+    int iagflg = 0;
+    int iahflg = 0;
 
-
-    print.level <- as.integer(print.level)
-    if (print.level < 0 || print.level > 2)
-        stop("'print.level' must be in {0,1,2}")
-    msg <- (1 + c(8, 0, 16))[1 + print.level]
-    if (!check.analyticals)  msg <- msg + (2 + 4)
-
-    FunctionInfo state(f, n);
-
-
-    //hessian = FALSE
-    typsize = rep(1, length(p)),
-    fscale = 1, print.level = 0, ndigit = 12, gradtol = 1e-06,
-    stepmax = max(1000 * sqrt(sum((p/typsize)^2)), 1000), steptol = 1e-06,
-    iterlim = 100, check.analyticals = TRUE
-
-    /* `typsize' : typical size of parameter elements */
-
-    typsiz = fixparam(CAR(args), &n);
-    args = CDR(args);
-
-    /* `fscale' : expected function size */
-
-    fscale = asReal(CAR(args));
-    if (ISNA(fscale)) error(_("invalid NA value in parameter"));
-    args = CDR(args);
-
-    /* `msg' (bit pattern) */
-    omsg = msg = asInteger(CAR(args));
-    if (msg == NA_INTEGER) error(_("invalid NA value in parameter"));
-    args = CDR(args);
-
-    ndigit = asInteger(CAR(args));
-    if (ndigit == NA_INTEGER) error(_("invalid NA value in parameter"));
-    args = CDR(args);
-
-    gradtl = asReal(CAR(args));
-    if (ISNA(gradtl)) error(_("invalid NA value in parameter"));
-    args = CDR(args);
-
-    stepmx = asReal(CAR(args));
-    if (ISNA(stepmx)) error(_("invalid NA value in parameter"));
-    args = CDR(args);
-
-    steptol = asReal(CAR(args));
-    if (ISNA(steptol)) error(_("invalid NA value in parameter"));
-    args = CDR(args);
-
-    /* `iterlim' (def. 100) */
-    itnlim = asInteger(CAR(args));
-    if (itnlim == NA_INTEGER) error(_("invalid NA value in parameter"));
-
-    state->R_env = rho;
-
-    /* force one evaluation to check for the gradient and hessian */
-    iagflg = 0;			/* No analytic gradient */
-    iahflg = 0;			/* No analytic hessian */
-    state->have_gradient = 0;
-    state->have_hessian = 0;
-    R_gradientSymbol = install("gradient");
-    R_hessianSymbol = install("hessian");
-
-    v = allocVector(REALSXP, n);
-    for (i = 0; i < n; i++) REAL(v)[i] = x[i];  //x: `p' : initial parameter value
-    SETCADR(state->R_fcall, v);                 //set initial parameter values for the callback fun
-    PROTECT(value = eval(state->R_fcall, state->R_env));
-
-    v = getAttrib(value, R_gradientSymbol);     //check if analysis-gradient is provided
-    if (v != R_NilValue) {
-        if (LENGTH(v) == n && (isReal(v) || isInteger(v))) {
-            iagflg = 1;
-            state->have_gradient = 1;                   //set state->have_gradient = 1
-            v = getAttrib(value, R_hessianSymbol);
-
-            if (v != R_NilValue) {
-                if (LENGTH(v) == (n * n) && (isReal(v) || isInteger(v))) {
-                    iahflg = 1;
-                    state->have_hessian = 1;            //set state->have_hessian = 1
-                } else {
-                    warning(_("hessian supplied is of the wrong length or mode, so ignored"));
-                }
-            }
-        } else {
-            warning(_("gradient supplied is of the wrong length or mode, so ignored"));
-        }
-    }
-    UNPROTECT(1); /* value: analysis-gradient/hessian */
-
-    // in the body of the R nlm
-    //  if (!check.analyticals)
-    //    msg <- msg + (2 + 4)
-    if (((msg/4) % 2) && !iahflg) { /* skip check of analytic Hessian */
+    if ((msg/4) % 2 && !iahflg) { /* skip check of analytic Hessian */
         msg -= 4;   //if analysis-hessian is not provided, msg -= 4
     }
-    if (((msg/2) % 2) && !iagflg) { /* skip check of analytic gradient */
+    if ((msg/2) % 2 && !iagflg) { /* skip check of analytic gradient */
         msg -= 2;   //if analysis-hessian is not provided, msg -= 2
     }
-    FT_init(n, FT_SIZE, state);  //allocate 5 Ftable in function_info *state
-    //    //typedef struct {
-    //    //  SEXP R_fcall;	      /* unevaluated call to R function */
-    //    //  SEXP R_env;	      /* where to evaluate the calls */
+
     //
-    //    //  int have_gradient;  //default=0
-    //    //  int have_hessian;   //default=0
+    // state
     //
-    //    //  int n;	             length of the parameter (x) vector
-    //    //  int FT_size;	      /* size of table to store computed function values */ initial=5
-    //    //  int FT_last;	      /* Newest entry in the table */, initial=-1
-    //    //  ftable *Ftable;
-    //    //} function_info;
+    FunctionInfo state(f, n);
+    state.createFTable(FT_SIZE);
 
     /* Plug in the call to the optimizer here */
 
-    method = 1;	/* Line Search */
+    int method = 1;	/* Line Search */
     // not expensive if analysis-Hessian is provided.
-    iexp = iahflg ? 0 : 1; /* Function calls are expensive */
-    dlt = 1.0;
+    int iexp = 1;   /* Function calls are expensive */
+    double dlt = 1.0;
 
-    xpls = (double*)R_alloc(n, sizeof(double));
-    gpls = (double*)R_alloc(n, sizeof(double));
-    a = (double*)R_alloc(n*n, sizeof(double));
-    wrk = (double*)R_alloc(8*n, sizeof(double));
+    double* gpls= new double[n];     // <--> on exit:  gradient at solution xpls
+    double* a= new double[n * n];    // a(n,n) --> workspace for hessian (or estimate)
+                                     // and its cholesky decomposition
+    double* wrk = new double[8 * n]; // wrk(n,8)   --> workspace
 
+    //<--> on exit:  function value at solution, xpls
+    double fpls = 100000.0;
     /*
      *	 Dennis + Schnabel Minimizer
      *
@@ -180,64 +166,75 @@ double nlm_simple(LossFunType f, double* xInit,
      *	 I think we always check gradients and hessians
      */
 
-    optif9(n, n, x, (fcn_p) fcn, (fcn_p) Cd1fcn, (d2fcn_p) Cd2fcn,
-           state, typsiz, fscale, method, iexp, &msg, ndigit, itnlim,
-           iagflg, iahflg, dlt, gradtl, stepmx, steptol, xpls, &fpls,
-           gpls, &code, a, wrk, &itncnt);
+    optif9(n, n, xInit, (fcn_p) fcn, (fcn_p) Cd1fcn, (d2fcn_p) Cd2fcn,
+           &state, c_typsize, c_fscale, method, iexp, &msg, c_ndigit, c_iterlim,
+           iagflg, iahflg, dlt, c_gradtol, c_stepmax, c_steptol, xOutput, &fpls,
+           gpls, resultCode, a, wrk, iterCount);
+    /*	provide complete interface to minimization package.
+ *	user has full control over options.
 
-    //optif9(int nr, int n, double *x, fcn_p fcn, fcn_p d1fcn, d2fcn_p d2fcn,
-    //       void *state, double *typsiz, double fscale, int method,
-    //       int iexp, int *msg, int ndigit, int itnlim,
-    //       int iagflg, int iahflg, double dlt, double gradtl, double stepmx, double steptl,
-    //       double *xpls, double *fpls, double *gpls, int *itrmcd, double *a,
-    //       double *wrk, int *itncnt)
+ * PARAMETERS :
+
+ *	nr	     --> row dimension of matrix
+ *	n	     --> dimension of problem
+ *	x(n)	 --> on entry: estimate to a root of fcn
+ *	fcn	     --> name of subroutine to evaluate optimization function
+ *			     must be declared external in calling routine
+ *				   fcn: r(n) --> r(1)
+ *	d1fcn	     --> (optional) name of subroutine to evaluate gradient
+ *			 of fcn.  must be declared external in calling routine
+ *	d2fcn	     --> (optional) name of subroutine to evaluate hessian of
+ *			 of fcn.  must be declared external in calling routine
+ *	state	    <--> information other than x and n that fcn,
+ *			 d1fcn and d2fcn requires.
+ *			 state is not modified in optif9 (but can be
+ *			 modified by fcn, d1fcn or d2fcn).
+ *	typsiz(n)    --> typical size for each component of x
+ *	fscale	     --> estimate of scale of objective function
+ *	method	     --> algorithm to use to solve minimization problem
+ *			   =1 line search
+ *			   =2 double dogleg
+ *			   =3 more-hebdon
+ *	iexp	     --> =1 if optimization function fcn is expensive to
+ *			 evaluate, =0 otherwise.  if set then hessian will
+ *			 be evaluated by secant update instead of
+ *			 analytically or by finite differences
+ *	msg	    <--> on input:  ( > 0) to inhibit certain automatic checks
+ *			 on output: ( < 0) error code; =0 no error
+ *	ndigit	     --> number of good digits in optimization function fcn
+ *	itnlim	     --> maximum number of allowable iterations
+ *	iagflg	     --> =1 if analytic gradient supplied
+ *	iahflg	     --> =1 if analytic hessian supplied
+ *	dlt	         --> trust region radius
+ *	gradtl	     --> tolerance at which gradient considered close
+ *			 enough to zero to terminate algorithm
+ *	stepmx	     --> maximum allowable step size
+ *	steptl	     --> relative step size at which successive iterates
+ *			 considered close enough to terminate algorithm
+ *	xpls(n)	    <--> on exit:  xpls is local minimum
+ *	fpls	    <--> on exit:  function value at solution, xpls
+ *	gpls(n)	    <--> on exit:  gradient at solution xpls
+ *	itrmcd	    <--	 termination code (in 0..5 ; 0 is "perfect");
+ *			see optcode() in optimize.c for meaning
+ *	a(n,n)	     --> workspace for hessian (or estimate)
+ *			 and its cholesky decomposition
+ *	wrk(n,8)     --> workspace
+ *	itncnt	    <--> iteration count
+ */
+
 
     if (msg < 0)
         opterror(msg);
-    if (code != 0 && (omsg&8) == 0)
-        optcode(code);
+    if (*resultCode != 0 && (omsg&8) == 0)
+        optcode(*resultCode);
 
-    if (want_hessian) {
-        PROTECT(value = allocVector(VECSXP, 6));
-        PROTECT(names = allocVector(STRSXP, 6));
-        fdhess(n, xpls, fpls, (fcn_p) fcn, state, a, n, &wrk[0], &wrk[n],
-               ndigit, typsiz);
-        for (i = 0; i < n; i++)
-            for (j = 0; j < i; j++)
-                a[i + j * n] = a[j + i * n];
-    }
-    else {
-        PROTECT(value = allocVector(VECSXP, 5));
-        PROTECT(names = allocVector(STRSXP, 5));
-    }
-    k = 0;
+
 
     SET_STRING_ELT(names, k, mkChar("minimum"));
     SET_VECTOR_ELT(value, k, ScalarReal(fpls));         //double: should return to python-end
     k++;
 
-    //out$estimate
-    SET_STRING_ELT(names, k, mkChar("estimate"));      //array[1-by-2k]: must return to python-end
-    SET_VECTOR_ELT(value, k, allocVector(REALSXP, n));
 
-    //xpls = (double*)R_alloc(n, sizeof(double));   //the memory of the return values is allocated in C-end
-    for (i = 0; i < n; i++)
-        REAL(VECTOR_ELT(value, k))[i] = xpls[i];  //xpls(n)	    <--> on exit:  xpls is local minimum
-    k++;
-
-    SET_STRING_ELT(names, k, mkChar("gradient"));
-    SET_VECTOR_ELT(value, k, allocVector(REALSXP, n));
-    for (i = 0; i < n; i++)
-        REAL(VECTOR_ELT(value, k))[i] = gpls[i];
-    k++;
-
-    if (want_hessian) {
-        SET_STRING_ELT(names, k, mkChar("hessian"));
-        SET_VECTOR_ELT(value, k, allocMatrix(REALSXP, n, n));
-        for (i = 0; i < n * n; i++)
-            REAL(VECTOR_ELT(value, k))[i] = a[i];
-        k++;
-    }
 
     SET_STRING_ELT(names, k, mkChar("code"));
     SET_VECTOR_ELT(value, k, allocVector(INTSXP, 1));
